9be6dc109e4cf7646763199b6e5fe15f694e8573
Cole Lin <cole.lin@hubdoc.com>
2019-03-08 12:00:40 -0500

    updated drawer icons and added feedback submission

--- client/src/containers/HomePage/Drawer.js
+++ client/src/containers/HomePage/Drawer.js
@@ -9,9 +9,15 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import { connect } from 'react-redux';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import ReportProblem from '@material-ui/icons/ReportProblem';
+import TableChart from '@material-ui/icons/TableChart';
+import ImportExport from '@material-ui/icons/ImportExport';
+import ListAlt from '@material-ui/icons/ListAlt';
+import LinkIcon from '@material-ui/icons/Link';
 
 import {
   handleDrawerOpen,
@@ -91,7 +97,22 @@ class AppDrawer extends React.Component {
         <Divider />
         <List>
           <ListItem component={Link} to={`/${HOME_PAGE}/${COURSE_TABLE}`} button key={0}>
+            <ListItemIcon>
+              <TableChart />
+            </ListItemIcon>
             <ListItemText primary="Course Table" />
+          </ListItem>
+          <ListItem component={Link} to={`/${HOME_PAGE}/${GRADUATION_CHECK}`} button key={3}>
+            <ListItemIcon>
+              <ListAlt />
+            </ListItemIcon>
+            <ListItemText primary="Graduation Requirement Check" />
+          </ListItem>
+          <ListItem button key={2}>
+            <ListItemIcon>
+              <ImportExport />
+            </ListItemIcon>
+            <ListItemText primary="Export Data" onClick={this.handleDataExportClick} />
           </ListItem>
           <ListItem
             button
@@ -101,14 +122,23 @@ class AppDrawer extends React.Component {
             rel="noopener"
             href="https://www.ouac.on.ca/guide/omsas-conversion-table"
           >
+            <ListItemIcon>
+              <LinkIcon />
+            </ListItemIcon>
             <ListItemText primary="GPA Converter Schema" />
           </ListItem>
-
-          <ListItem button key={2}>
-            <ListItemText primary="Export Data" onClick={this.handleDataExportClick} />
-          </ListItem>
-          <ListItem component={Link} to={`/${HOME_PAGE}/${GRADUATION_CHECK}`} button key={3}>
-            <ListItemText primary="Graduation Requirement Check" />
+          <ListItem
+            button
+            key={1}
+            component="a"
+            target="_blank"
+            rel="noopener"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdoCAn65lStAvMXJH9eUaLbgiD-2YnIUAJPpXGoIVC6nQIvXQ/viewform"
+          >
+            <ListItemIcon>
+              <ReportProblem />
+            </ListItemIcon>
+            <ListItemText primary="Report an Issue" />
           </ListItem>
         </List>
         <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={this.handleDataExportClose}>
